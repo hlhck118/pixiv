ac24c4496244ce57da021eaa1d97b1ff5c0f16b3
Tien Thanh <thanhvt1609@gmail.com>
2015-03-16 17:16:56 +0700

    fix basic layout and devise with new css framework and slim

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,20 @@
+function init(){
+  $(".button-collapse").sideNav();
+
+  if (flash_message !== ''){
+    toast(flash_message, 3000, 'grey lighten-5')
+  }
+
+  $('input[type="text"]').each(function(index, el){
+    if ($(this).val() != '') {
+      $("label[for='"+this.id+"']").addClass('active');
+    }
+  });
+
+  $('textarea').each(function(index, el){
+    if ($(this).val() != ''){
+      $(this).prev('label').addClass('active');
+      $("label[for='"+this.id+"']").addClass('active');
+    }
+  });
+};
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def flash_html
+    return "" if flash.empty?
+    div = ""
+    div << content_tag(:div, class: "alert") do
+      p = ""
+      flash.each do |type, msg|
+        p << content_tag(:p) do
+          content_tag(:strong, msg, class: flash_type_to_css_class(type))
+        end
+      end
+      p.html_safe
+    end
+    div.html_safe
+  end
+
+  def flash_type_to_css_class(type)
+    css_class = ""
+    if type == "success" || type == "notice"
+      css_class = "green"
+    elsif type == "info"
+      css_class = "blue"
+    elsif type == "danger" || type == "alert"
+      css_class = "red"
+    elsif type == "warning"
+      css_class = "orange"
+    end
+    css_class += "-text"
+  end
 end
